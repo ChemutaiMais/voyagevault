a6eb9db3438d7a76d6f1edebb2c3b38b464fa8bd
ChemutaiMais <chemutaib3ryl@gmail.com>
2026-03-14 18:05:19 +0300

    Create App.jsx React component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Voyage Vault!</h1>
+            <p>Current Date and Time: 2026-03-14 15:03:08 UTC</p>
+        </div>
+    );
+};
+
+export default App;
